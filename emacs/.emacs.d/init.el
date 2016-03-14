559ab9f2184c2e12db20eb7c8c48263480f7ce93
Danielle Kefford <daniellek@Jumpclouds-MacBook-Pro.local>
2016-03-14 10:09:26 -0400

    Updated emacs config.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,6 +8,10 @@
 (mouse-wheel-mode t)
 (setq mouse-wheel-scroll-amount '(1 ((shift) 1) ((control) . nil)))
 
+(windmove-default-keybindings)
+
+(global-linum-mode)
+
 (global-set-key [f8] 'neotree-toggle)
 (setq-default neo-show-hidden-files t)
 
@@ -16,3 +20,18 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 (add-hook 'after-init-hook 'global-company-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(neo-dir-link-face ((t (:foreground "yellow"))))
+ '(neo-file-link-face ((t (:foreground "white"))))
+ '(neo-root-dir-face ((t (:foreground "brightmagenta")))))
